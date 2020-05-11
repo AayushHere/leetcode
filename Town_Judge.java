2b0c3b900c696f83607747a5abed1acb750db020
dhiman-12 <aayush.dhiman7777@gmail.com>
2020-05-11 11:18:54 +0530

    Town_Judge

---
+++ Town_Judge.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        
+       ArrayList<Integer> noDup = new ArrayList<Integer>();
+        int i=0,j=0, c=0;
+        for(i=0; i<trust.length ;i++){
+            if(!noDup.contains(trust[i][0])){
+                noDup.add(trust[i][0]);
+            }
+        }
+        int check=0;
+        for(i=1;i<=N;i++){
+            if(!noDup.contains(i)){
+                check=i;
+                break;
+            }
+        }
+        for(i=0;i<noDup.size();i++){
+            for(j=0;j<trust.length;j++){
+                if(noDup.get(i)==trust[j][0]){
+                    if(trust[j][1]==check){
+                        c++;
+                        break;
+                    }
+                }
+            }
+        }
+        if(c==N-1)
+            return check;
+        else    
+            return -1;
+        
+    }
+}
